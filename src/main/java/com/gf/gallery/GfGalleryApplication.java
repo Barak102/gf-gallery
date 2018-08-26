aa069ef3ceffe1c240c28827662376aad1529d11
Barak <yosef96385>
2018-08-27 00:17:08 +0300

    Upload GF Gallery

---
+++ src/main/java/com/gf/gallery/GfGalleryApplication.java
@@ -0,0 +1,12 @@
+package com.gf.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfGalleryApplication.class, args);
+	}
+}
